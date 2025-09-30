90ce6b682fe386c0b2798e863c51b33682e0587a
Pramod Morthala <163892846+pramodmorthala-13@users.noreply.github.com>
2025-09-30 17:13:44 +0530

    Update polyfill_Map_Filter_Reduce.js
    polyfill for filter

--- JSRun/polyfill_Map_Filter_Reduce.js
+++ JSRun/polyfill_Map_Filter_Reduce.js
@@ -15,3 +15,25 @@ let arr1=[1,2,3];
 
 const res = arr1.myMap((a,i)=>a+1)
 console.log(res)
+
+
+
+//Polyfill for Filter
+
+Array.prototype.myFilter = function(cb){
+    if(typeof cb != "function"){
+        throw new TypeError(cb + "is not a function")
+    }
+    let res =[];
+    for(let i=0; i<this.length;i++){
+        if(cb(this[i],i,this)){
+        res.push(this[i])
+        }
+    }
+    return res
+}
+
+let arr2=[1,2,3];
+
+const resFil = arr2.myFilter((a,i)=>a>2)
+console.log(resFil)
